a903f60222832f99b80e1255337ae17e2dbe4b4b
student <christian.lobaugh24@alumni.loyola.ca>
2021-11-26 22:53:21 -0500

    ore tags added

--- src/main/java/com/charizard1596/galvoritetech/block/ModBlocks.java
+++ src/main/java/com/charizard1596/galvoritetech/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package com.charizard1596.galvoritetech.block;
 
 import com.charizard1596.galvoritetech.GalvoriteTech;
+import com.charizard1596.galvoritetech.item.ModCreativeModeTab;
 import com.charizard1596.galvoritetech.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
@@ -19,16 +20,26 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, GalvoriteTech.MOD_ID);
     public static final RegistryObject<Block> GALVORITE_ORE = registerBlock("galvorite_ore",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(30f)));
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(10f).requiresCorrectToolForDrops()), ModCreativeModeTab.GALVORITE_TAB);
+
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
+    private static <T extends Block> void registerBlockItem(String name, Supplier<T> block, CreativeModeTab tab) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    }
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn);
         return toReturn;
     }
     private static <T extends Block> void registerBlockItem(String name, Supplier<T> block) {
-        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(ModCreativeModeTab.GALVORITE_TAB)));
     }
     public static void register(IEventBus eventBus) {
         BLOCKS.register((eventBus));
     }
 }
+
